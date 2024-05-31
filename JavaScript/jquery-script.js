33ac24b4c7837115e8d41571890ad496cbde7298
Rômulo França Dev <romulo.franca03@gmail.com>
2024-05-31 10:39:48 -0300

    commit

---
+++ JavaScript/jquery-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#mobile_btn').click(function(){
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn i').toggleClass('fa-bars fa-x');
+    });
+});
+
